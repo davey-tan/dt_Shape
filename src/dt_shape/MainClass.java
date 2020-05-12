4705f2a0e373e923e48840e554757ab4d86c775f
Davey <davey_tan@tutanota.com>
2020-05-12 19:17:44 +0700

    Initial testing

--- src/dt_shape/MainClass.java
+++ src/dt_shape/MainClass.java
@@ -3,7 +3,19 @@ package dt_shape;
 public class MainClass {
 
     public static void main(String[] args) {
-        
+        Shape shape1 = new Shape();
+        System.out.println(shape1);
+        Shape shape2 = new Shape("green", true);
+        System.out.println(shape2);
+
+        System.out.println("");
+
+        Circle circle1 = new Circle();
+        System.out.println(circle1);
+        Circle circle2 = new Circle(7);
+        System.out.println(circle2);
+        System.out.println("Area: " + circle2.getArea());
+        System.out.println("Perimeter: " + circle2.getPerimeter());
     }
-    
+
 }
