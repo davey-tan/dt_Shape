92b8146c523bc1e2319a220f44c65a201b2ec5a6
Davey <davey_tan@tutanota.com>
2020-05-12 18:45:18 +0700

    initial commit

---
+++ src/dt_shape/Shape.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dt_shape;
+
+/**
+ *
+ * @author Crestfall
+ */
+public class Shape {
+
+    public String getColor() {
+        return shapeColor;
+    }
+
+    public void setColor(String color) {
+        shapeColor = color;
+    }
+
+    public boolean isFilled() {
+        return shapeFilled;
+    }
+
+    public void setFilled(boolean filled) {
+        shapeFilled = filled;
+    }
+
+    private String shapeColor = "red";
+    private boolean shapeFilled = true;
+
+    public Shape() {
+
+    }
+}
